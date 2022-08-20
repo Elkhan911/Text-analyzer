11501c6e1535d537762b7281eb70ee0c37d1e889
Elkhan911 <955pro@mail.ru>
2022-08-20 14:13:04 +0500

    first task html

---
+++ script.js
@@ -0,0 +1,10 @@
+// Variables of Task one
+const input1Task1 = document.querySelector("#_input1Task1");
+const input2Task1 = document.querySelector("#_input2Task1");
+const input3Task1 = document.querySelector("#_input3Task1");
+const span1 = document.querySelector("#_span1");
+const span2 = document.querySelector("#_span2");
+const span3 = document.querySelector("#_span3");
+const answerBtn1 = document.querySelector("#_answerBtn1");
+const resetBtn1 = document.querySelector("#_resetBtn1");
+const spanAnswerTask1 = document.querySelector("#_spanAnswerTask1");
