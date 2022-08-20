1bf9b217a57a79b1336baef03dee1b228d9b7b19
Elkhan911 <955pro@mail.ru>
2022-08-20 16:22:18 +0500

    problems with cycle js

--- script.js
+++ script.js
@@ -1,10 +1,47 @@
-// Variables of Task one
-const input1Task1 = document.querySelector("#_input1Task1");
-const input2Task1 = document.querySelector("#_input2Task1");
-const input3Task1 = document.querySelector("#_input3Task1");
+// Variables of Task One
+const textZone1 = document.querySelector("#_textZone1");
 const span1 = document.querySelector("#_span1");
+// Variables of Task Two
+const textZone2 = document.querySelector("#_textZone2");
 const span2 = document.querySelector("#_span2");
+// Variables of Task Three
+const textZone3 = document.querySelector("#_textZone3");
 const span3 = document.querySelector("#_span3");
-const answerBtn1 = document.querySelector("#_answerBtn1");
-const resetBtn1 = document.querySelector("#_resetBtn1");
-const spanAnswerTask1 = document.querySelector("#_spanAnswerTask1");
+
+textZone1.addEventListener("blur", function () {
+  let str = textZone1.value;
+  let arr1 = str.split(" ");
+
+  let i = 0;
+  for (i = 0; i < arr1.length; i++) {}
+  span1.textContent = i;
+});
+
+textZone2.addEventListener("blur", function () {
+  let str = textZone2.value;
+  let arr2 = str.split("");
+
+  let i = 0;
+
+  for (i = 0; i < arr2.length; i++) {}
+  span2.textContent = i;
+});
+
+textZone3.addEventListener("blur", function () {
+  let str = textZone3.value;
+  let arr3 = str.split("");
+
+  let k = 0;
+
+  textZone3.addEventListener("keydown", function (event) {
+    for (let i = 0; i, arr3.length; i++) {
+      k++;
+      if (event.key == "" || event.key == " ") {
+        console.log("probel");
+        k = k + 0;
+      }
+    }
+  });
+
+  span3.textContent = k;
+});
